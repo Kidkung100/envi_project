cecf8acc8d9b19939cecee2772ed66bd7d525b9d
Kit <kidkung1@gmail.com>
2025-09-22 05:02:38 +0700

    Update: เพิ่มฟีเจอร์ X / แก้ไข bug Y

--- frontend/lib/fullscreen_helper_web.dart
+++ frontend/lib/fullscreen_helper_web.dart
@@ -1,7 +1,5 @@
-// ignore: avoid_web_libraries_in_flutter
 import 'dart:html' as html;
 
-/// เรียกเมื่อมีการเปลี่ยนแปลง Fullscreen (เข้า/ออก)
 void initFullscreenListener(Function(bool) onChange) {
   html.document.onFullscreenChange.listen((event) {
     final isFullscreen = html.document.fullscreenElement != null;
@@ -9,7 +7,6 @@ void initFullscreenListener(Function(bool) onChange) {
   });
 }
 
-/// สลับ fullscreen
 void toggleFullscreen(bool isFullscreen) {
   if (isFullscreen) {
     html.document.exitFullscreen();
